edf977975a7f09df8dd5759fa861d63269607590
Tanjila Chowdhury Orpe <orpetanjila@gmail.com>
2023-03-01 15:02:03 +0600

    2nd

--- js.js
+++ js.js
@@ -4,9 +4,32 @@
 //     const data = await res.json();
 //     console.log(data);
 // }
-const func = () =>{
-    fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+const func = (phone) =>{
+    fetch(`https://openapi.programming-hero.com/api/phones?search=${phone}`)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => displayPhone(data.data))
+}
+const body = document.getElementById('container');
+const displayPhone = (display) =>{
+    body.innerHTML = '';
+    display.forEach(item => {
+        console.log(item);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+            <div class="card">
+                <img src="${item.image}" class="card-img-top img-fluid" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${item.phone_name}</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+            </div>`
+        body.appendChild(div);
+    });
+    document.getElementById('btn-search').addEventListener('click',() =>{
+        const searchPhone = document.getElementById('input').value;
+        console.log(searchPhone);
+        func(searchPhone);
+    })
 }
 func();
